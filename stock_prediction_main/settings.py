87e4dd55ef59cbf525d8f98c1080aba121419a8b
anubhav <anubhavsaxenavibhu@gmail.com>
2025-07-05 02:27:44 +0530

    add

--- stock_prediction_main/settings.py
+++ stock_prediction_main/settings.py
@@ -27,7 +27,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env('SECRET_KEY')
 
 DEBUG = env('DEBUG')
-ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['localhost'])
+ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = env.list('CSRF_TRUSTED_ORIGINS', default=[])
 
 
